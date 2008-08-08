69e674d45440ffc68543b070dd2297a2307be4bf
Adam Czachorowski <gislan@utumno.pl>
2008-08-08 04:16:51 +0200

    self contact fixed transports bugs major rosterbuilder cleanup minor cleanups to other files

--- src/psiaccount.cpp
+++ src/psiaccount.cpp
@@ -5090,5 +5090,8 @@ QList<PsiAccount::xmlRingElem> PsiAccount::dumpRingbuf()
 	return d->dumpRingbuf();
 }
 
+UserListItem PsiAccount::self() const {
+	return d->self;
+}
 
 #include "psiaccount.moc"
